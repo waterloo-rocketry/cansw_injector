d2b476c20d2baee1254631fc5f77d1c8730455b3
Alex Mihaila <am@objectline.com>
2018-11-28 18:11:50 -0500

    Make it blink so I know it's not dead

--- main.c
+++ main.c
@@ -7,7 +7,12 @@ int main(int argc, char** argv) {
     led_init();
     injector_init();
     
-    while (1);
+    while (1) {
+        BLUE_LED_ON();
+        __delay_ms(200);
+        BLUE_LED_OFF();
+        __delay_ms(200);
+    }
     return (EXIT_SUCCESS);
 }
 
